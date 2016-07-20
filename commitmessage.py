713898e91c8e42968942848c86b3cbea834f4bf3
hstormer <hstormer@ualberta.com>
2016-07-20 13:30:52 -0600

    Added a save location feature.

--- commitmessage.py
+++ commitmessage.py
@@ -12,6 +12,8 @@ db = client.github
 # This is a thing containing all the documents somehow
 cursor = db.commits.find()
 counter = 0
+save_location = 4719
+location = 0
 name = 'Hannah'
 message = None
 parents = None
@@ -22,80 +24,86 @@ print "============================================"
 
 # For each document in cursor...
 for document in cursor:
-	# This is the commit comment
-	message = document.get("commit")
-	message = message.get("message")
-	# This is the link to the comment
-	link = document.get("html_url")
-	patch = document.get("files")
-	if patch != [] and patch != None:
-		patch = patch[0]
-		patch = patch.get("patch")
-	parents = document.get("parents")
-	if parents != [] and parents != None:
-		parents = parents[0]
-		parents = parents.get("html_url")
-	length = document.get("files")
-	if length != [] and length != None:
-		length = length[0]
-		additions = length.get("additions")
-		deletions = length.get("deletions")
-	file1 = document.get("files")
-	if file1 != [] and file1 != None:
-		file1 = file1[0]
-		file1 = file1.get("filename")	
-		if '.py' in file1:
-			# For each of the keywords we are looking for...
-			for i in things:
-				# If it is in the message...
-				if i in message:
-					print "++++++++++++++++"
-					print patch
-					print "++++++++++++++++"
-					counter += 1
-					if counter == 4:
-						counter = 1
-					if counter == 1:
-						print "This is Hannah's."
-						name = 'Monica'
-					if counter == 2:
-						print "This is Monica's."
-						name = 'Eisha'
-					if counter == 3:
-						print "This is Eisha's."
-						name = 'Hannah'
-					print "\nCommit Message: %s" % message
-					print "This is the link to the commit: \n%r" % link
-					print "This is the link to the previous commit: \n%r" % parents
-					print "This is the file being edited: %r" % file1
-					print "Additions: %r" % additions
-					print "Deletions: %r" % deletions
-					txt = open(filename, 'a+')
-					if str(link) in txt.read():
-							print "WE HAVE ADDED THIS ALREADY!"
-					txt.close()
-					answer = raw_input("Do you want to include this file as an entry? y/n ")
-					if answer == 'y':
+	if save_location == location:
+		save_location += 1
+		location += 1
+		# This is the commit comment
+		message = document.get("commit")
+		message = message.get("message")
+		# This is the link to the comment
+		link = document.get("html_url")
+		patch = document.get("files")
+		if patch != [] and patch != None:
+			patch = patch[0]
+			patch = patch.get("patch")
+		parents = document.get("parents")
+		if parents != [] and parents != None:
+			parents = parents[0]
+			parents = parents.get("html_url")
+		length = document.get("files")
+		if length != [] and length != None:
+			length = length[0]
+			additions = length.get("additions")
+			deletions = length.get("deletions")
+		file1 = document.get("files")
+		if file1 != [] and file1 != None:
+			file1 = file1[0]
+			file1 = file1.get("filename")	
+			if '.py' in file1:
+				# For each of the keywords we are looking for...
+				for i in things:
+					# If it is in the message...
+					if i in message:
+						print "++++++++++++++++"
+						print patch
+						print "++++++++++++++++"
+						counter += 1
+						if counter == 4:
+							counter = 1
+						if counter == 1:
+							print "This is Hannah's."
+							name = 'Monica'
+						if counter == 2:
+							print "This is Monica's."
+							name = 'Eisha'
+						if counter == 3:
+							print "This is Eisha's."
+							name = 'Hannah'
+						print "\nCommit Message: %s" % message
+						print "This is the link to the commit: \n%r" % link
+						print "This is the link to the previous commit: \n%r" % parents
+						print "This is the file being edited: %r" % file1
+						print "Additions: %r" % additions
+						print "Deletions: %r" % deletions
+						print "Save location: %d" % (save_location - 1)
 						txt = open(filename, 'a+')
-						CommitMistakes = str(parents)
-						CommitCorrections = str(link)
-						mistake = raw_input("Type a message for Mistake: ")
-						tag = raw_input("Type the Tag: ")
-						length = raw_input("Type the length of changes: ")
-						if txt.readline(1) != "*":
-							txt.write("*" + "\n" "Commit Mistakes:" + "\n" + 								CommitMistakes + "\n" + "Commit Corrections:" + "\n" + 								CommitCorrections + "\n" + "Mistakes:" + "\n" + mistake + 								"\n" + "Tags:" + "\n" + tag + "\n" + "Length:" + "\n" + 							length)
-						else:
-							txt.write("\n" + "*" + "\n" "Commit Mistakes:" + "\n" + 							CommitMistakes + "\n" + "Commit Corrections:" + "\n" + 								CommitCorrections + "\n" + "Mistakes:" + "\n" + mistake + 								"\n" + "Tags:" + "\n" + tag + "\n" + "Length:" + "\n" + 							length)
+						if str(link) in txt.read():
+								print "WE HAVE ADDED THIS ALREADY!"
 						txt.close()
-						print "\n============================================"
-						print "Finding another file just for %s..." % name
-						print "============================================"
-					else:
-						print "\n============================================"
-						print "Finding another file just for %s..." % name
-						print "============================================"
-					# Don't print it again if it also contains another thing
-					break
+						answer = raw_input("Do you want to include this file as an entry? y/n ")
+						if answer == 'y':
+							txt = open(filename, 'a+')
+							CommitMistakes = str(parents)
+							CommitCorrections = str(link)
+							mistake = raw_input("Type a message for Mistake: ")
+							tag = raw_input("Type the Tag: ")
+							length = raw_input("Type the length of changes: ")
+							if txt.readline(1) != "*":
+								txt.write("*" + "\n" "Commit Mistakes:" + "\n" + 								CommitMistakes + "\n" + "Commit Corrections:" + "\n" + 								CommitCorrections + "\n" + "Mistakes:" + "\n" + mistake + 								"\n" + "Tags:" + "\n" + tag + "\n" + "Length:" + "\n" + 							length)
+							else:
+								txt.write("\n" + "*" + "\n" "Commit Mistakes:" + "\n" + 							CommitMistakes + "\n" + "Commit Corrections:" + "\n" + 								CommitCorrections + "\n" + "Mistakes:" + "\n" + mistake + 								"\n" + "Tags:" + "\n" + tag + "\n" + "Length:" + "\n" + 							length)
+							txt.close()
+							print "\n============================================"
+							print "Finding another file just for %s..." % name
+							print "============================================"
+						else:
+							print "\n============================================"
+							print "Finding another file just for %s..." % name
+							print "============================================"
+						# Don't print it again if it also contains another thing
+						break
+	else: 
+		location += 1
 
 	
 
