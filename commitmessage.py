50a301ff8bedd6c0edbec62d8bd48cb73c999732
hstormer <hstormer@ualberta.com>
2016-07-20 10:50:07 -0600

    Made it automatically add entries to the test.txt file.

--- commitmessage.py
+++ commitmessage.py
@@ -1,12 +1,20 @@
 # This initiates the thing that connects to the server thing that does a thing
 import pymongo
 from pymongo import MongoClient
+
+from sys import argv
+script, filename = argv
+CommitMistakes = ''
+CommitCorrections = ''
+
 client = MongoClient('10.13.10.124')
 db = client.github
 # This is a thing containing all the documents somehow
 cursor = db.commits.find()
 message = None
 things = ["indentation", "typo", "bracket", "misspell", "mistake", "syntax", "correct", "corrected", "missed", "minor fixes", "minor changes", "redundant"]
+print "============================================"
+print "Finding another file just for you..."
 print "============================================"
 
 # For each document in cursor...
@@ -21,7 +29,7 @@ for document in cursor:
 		patch = patch[0]
 		patch = patch.get("patch")
 	file1 = document.get("files")
-	if file1 != [] and file1 != None:
+	if patch != [] and patch != None:
 		file1 = file1[0]
 		file1 = file1.get("filename")	
 		if '.py' in file1:
@@ -29,15 +37,32 @@ for document in cursor:
 			for i in things:
 				# If it is in the message...
 				if i in message:
-					print "Commit Message: %r" % message
+					print "Commit Message: %s" % message
 					print "\n++++++++++++++++"
 					print patch
 					print "++++++++++++++++\n"
 					print "This is the link to the commit: \n%r" % link
 					print "This is the file being edited: %r" % file1
-					print "\n============================================"
-					answer = raw_input("Press something to continue printing...")
-					print "============================================\n"
-					# Don't print it again if it also contains another thing.
+					answer = raw_input("Do you want to include this file as an entry? y/n ")
+					if answer == 'y':
+						txt = open(filename, 'a+')
+						CommitMistakes = 'None'
+						CommitCorrections = str(link)
+						mistake = raw_input("Type a message for Mistake: ")
+						tag = raw_input("Type the Tag: ")
+						length = raw_input("Type the Length (long/short): ")
+						if txt.readline(1) != "*":
+							txt.write("*" + "\n" "Commit Mistakes:" + "\n" + CommitMistakes + 								"\n" + "Commit Corrections:" + "\n" + CommitCorrections + "\n" + 								"Mistakes:" + "\n" + mistake + "\n" + "Tags:" + "\n" + tag + "\n" 								+ "Length:" + "\n" + length)
+						else:
+							txt.write("\n" + "*" + "\n" "Commit Mistakes:" + "\n" + 							CommitMistakes + "\n" + "Commit Corrections:" + "\n" + 								CommitCorrections + "\n" + "Mistakes:" + "\n" + mistake + "\n" + 								"Tags:" + "\n" + tag + "\n" + "Length:" + "\n" + length)
+						txt.close()
+						print "\n============================================"
+						print "Finding another file just for you..."
+						print "============================================"
+					else:
+						print "\n============================================"
+						print "Finding another file just for you..."
+						print "============================================"
+					# Don't print it again if it also contains another thing
 					break
 
