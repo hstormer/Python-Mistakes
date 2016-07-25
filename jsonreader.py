c012deda0158c3c18b45e46be8b2d52526069e76
Eisha Ahmed <you@example.com>
2016-07-25 10:04:49 -0600

    switched name

--- jsonreader.py
+++ jsonreader.py
@@ -6,7 +6,7 @@ item = None
 # Run the program with the name of the file to open after the script name (in this case test.txt)
 script, open_file = argv
 
-with open('jsontest.json') as data_file:
+with open('no.json') as data_file:
 	data = json.load(data_file)
 
 print data[0]
