b17c885a2163dca02c7cf7259ae125a85f90495a
hstormer <hstormer@ualberta.com>
2016-07-26 09:54:51 -0600

    updated reader

--- jsonreader.py
+++ jsonreader.py
@@ -4,13 +4,15 @@ import json
 item = None
 
 # Run the program with the name of the file to open after the script name (in this case test.txt)
-script, open_file = argv
+file1 = raw_input("Which file do you want to read?: ")
 
-with open('no.json') as data_file:
+
+with open(file1) as data_file:
 	data = json.load(data_file)
 
 print data[0]
 item = data[0]
+print len(data)
 print item
 print item.get("Commit Mistakes")
 
