a57867020a598103dde2a6da40bf25999f9b6119
hstormer <hstormer@ualberta.com>
2016-07-26 14:10:03 -0600

    Deleted repeats and updated the no file so it doesn't have : in commit corrections. Also finished the duplicate test file

--- repeattest.py
+++ repeattest.py
@@ -1,48 +1,39 @@
-#this program goes through the yes.json or no.json file to check for any repeats
-
-from sys import argv
 import json
-#import finalyes.json
-from pymongo import MongoClient
-script, open_file = argv
-client = MongoClient()
-db = client.github
 
-with open('Hello.json') as f:
-	entries = json.load(f)
-first_list = []
-new_list = []
-x = entries.pop()
-new_list.append(x)
+ofile = raw_input("What file do you want to check for duplicates?")
 
+ofile = open(ofile, 'a+')
+entries = json.load(ofile)
 
-for i in entries:
-	link = i.get('Commit Corrections')
-	#print link
-	first_list.append(i)
-	for k in new_list:
-		#print k.get('Commit Corrections')
-		lonk = k.get('Commit Corrections')
-		if link in lonk:
-			print "We found a duplicate"
-			print k.get('Commit Corrections')
-			break
-		elif lonk in link:
-			print "We found a duplicate"
-			print k.get('Commit Corrections')
+nlist = []
+nlist.append(entries[0])
+link = None
+duplicates = 0
+
+for entry in entries:
+	link = entry.get('Commit Corrections')
+	counter = 0
+	for item in nlist:
+		link2 = item.get('Commit Corrections')
+		if link in link2:
+			print "We found a duplicate!"
+			duplicates += 1
+			counter = 0
+			print link
+			print link2
 			break
 		else:
-			new_list.append(i)
-			print "Adding to the list"
-			break
-print first_list
-print new_list
-print len(first_list) + 1
-print len(new_list)
+			counter += 1
+	if counter == len(nlist):
+		nlist.append(entry)
 
-#f = open(finalyes.json, 'r+') 
-#text = f.read()
-#f.write(
+duplicates -= 1
+print "We found %d duplicates." % duplicates
+
+ofile.seek(0)
+ofile.truncate()
+with ofile as outfile:
+	json.dump(nlist, outfile, indent = 2)
 
 			
 	
