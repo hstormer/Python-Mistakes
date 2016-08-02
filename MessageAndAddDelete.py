728b6d2e9868082477593deb91d88b451690954f
hstormer <hstormer@ualberta.com>
2016-08-02 14:35:51 -0600

    Made the message and add delete file work (hopefully) and made it write to a new file. Also updated the reader to add colons and delete keys so that all the entries are the same!

--- MessageAndAddDelete.py
+++ MessageAndAddDelete.py
@@ -2,14 +2,19 @@ import pymongo
 import json
 from random import randint
 from pymongo import MongoClient
+from sys import argv
+script, ofile, savefile = argv
 
 client = MongoClient()
 db = client.github
 cursor = db.commits.find()
 
-ofile = raw_input("What file would you like to add commit messages to?")
-ofile = open(ofile, 'a+')
+#Cannot write to this
+ofile = open(ofile, 'r')
+#Will delete everything in the file
+savefile = open(savefile, 'w')
 data = json.load(ofile)
+outdata = []
 
 done = len(data)
 current = -1
@@ -17,6 +22,33 @@ counter = 0
 link = ''
 entry = None
 yay = ["I FOUND ONE!", "Wow, I found one!", "Yay! I found one!", "Guess what? I found one!", "I found The One!!!"]
+
+def housecleaning(l):
+	finalist = []
+	list1 = []
+	finish = []
+	templist = []
+	for line in l:
+		list1.append(line)
+	for i in list1:
+		templist.append(i)
+		if i.encode('utf8') == '\n':
+			str1 = ''.join(templist)
+			templist = []
+			finalist.append(str1)
+	str1 = ''.join(templist)
+	finalist.append(str1)
+	finalist.pop(0)
+	list1 = []
+	for i in finalist:
+		list1 = []
+		for line in i:
+			list1.append(line)
+		templist = []
+		if str(list1[0])== '+' or str(list1[0]) == '-':
+			str1 = ''.join(list1)
+			finish.append(str1)
+	return finish
 
 for document in cursor:
 	lonk = document.get("html_url")
@@ -28,7 +60,6 @@ for document in cursor:
 				message = message.get("message")
 				link = data[current].get("Commit Corrections")
 				item["Message:"] = message
-				print yay[randint(0,3)]
 				length = document.get("files")
 				if length != [] and length != None:
 					length = length[0]
@@ -36,15 +67,21 @@ for document in cursor:
 					deletions = length.get("deletions")
 					item["Additions:"] = additions
 					item["Deletions:"] = deletions
+					patch = document.get("files")
+					if patch != [] and patch != None:
+						patch = patch[0]
+						patch = patch.get("patch")
+						patch = housecleaning(patch)
+						item["Changes:"] = patch
+						print yay[randint(0,4)]
+						outdata.append(item)
 				break
 	counter += 1
 	#This is based on save location. If it is more than this, then change it.
 	if counter == 100000:
 		break
 
-ofile.seek(0)
-ofile.truncate()
-with ofile as outfile:
-	json.dump(data, outfile, indent = 2)
+with savefile as outfile:
+	json.dump(outdata, savefile, indent = 2)
 
 	
--- jsonreader.py
+++ jsonreader.py
@@ -6,14 +6,127 @@ item = None
 # Run the program with the name of the file to open after the script name (in this case test.txt)
 file1 = raw_input("Which file do you want to read?: ")
 
-
 with open(file1) as data_file:
 	data = json.load(data_file)
+
+store = None
+
+for i in data:
+	if i.get("Additions") != None:
+		store = i.get("Additions")
+		i.pop("Additions")
+		i["Additions:"] = store
+		print "Changed one Additions to Additions:"
+
+for i in data:
+	if i.get("Deletions") != None:
+		store = i.get("Deletions")
+		i.pop("Deletions")
+		i["Deletions:"] = store
+		print "Changed one Deletions to Deletions:"
+
+for i in data:
+	if i.get("Changes") != None:
+		store = i.get("Changes")
+		i.pop("Changes")
+		i["Changes:"] = store
+		print "Changed one Changes to Changes:"
+
+for i in data:
+	if i.get("Length") != None:
+		store = i.get("Length")
+		i.pop("Length")
+		i["Length:"] = store
+		print "Changed one Length to Length:"
+
+for i in data:
+	if i.get("Commit Corrections") != None:
+		store = i.get("Commit Corrections")
+		i.pop("Commit Corrections")
+		i["Commit Corrections:"] = store
+		print "Changed one Commit Corrections to Commit Corrections:"
+
+
+for i in data:
+	if i.get("Commit Mistakes") != None:
+		store = i.get("Commit Mistakes")
+		i.pop("Commit Mistakes")
+		i["Commit Mistakes:"] = store
+		print "Changed one Commit Mistakes to Commit Mistakes:"
+
+for i in data:
+	if i.get("Commit Mistake") != None:
+		store = i.get("Commit Mistake")
+		i.pop("Commit Mistake")
+		i["Commit Mistakes:"] = store
+		print "Changed one Commit Mistake to Commit Mistakes:"
+
+for i in data:
+	if i.get("Tags") != None:
+		store = i.get("Tags")
+		i.pop("Tags")
+		i["Tags:"] = store
+		print "Changed one Tags to Tags:"
+
+for i in data:
+	if i.get("Mistakes") != None:
+		store = i.get("Mistakes")
+		i.pop("Mistakes")
+		i["Mistakes:"] = store
+		print "Changed one Mistakes to Mistakes:"
+
+for i in data:
+	if i.get("Mistake") != None:
+		store = i.get("Mistake")
+		i.pop("Mistake")
+		i["Mistakes:"] = store
+		print "Changed one Mistake to Mistakes:"
+
+for i in data:
+	if i.get("Why Not") != None:
+		store = i.get("Why Not")
+		i.pop("Why Not")
+		i["Why Not:"] = store
+		print "Changed one Why Not to Why Not:"
+
+for i in data:
+	if i.get("Keyword:") != None:
+			del i["Keyword:"]
+
+
+for i in data:
+	if i.get("Mistakes:") != None:
+			del i["Mistakes:"]
+
+for i in data:
+	if i.get("Why Not:") != None:
+			del i["Why Not:"]
+
+
+for i in data:
+	print i
+	assert "Additions:" in i
+	assert "Deletions:" in i
+	assert "Changes:" in i
+	assert "Length:" in i
+	assert "Commit Mistakes:" in i
+	assert "Commit Corrections:" in i
+	assert "Tags:" in i
+	assert "Message:" in i
+	assert "Why Not:" not in i
+	assert "Mistakes:" not in i
+	assert len(i) == 8
+
 
 print data[0]
 item = data[0]
 print len(data)
 print item
 print item.get("Commit Mistakes")
+
+
+file1 = open(file1, 'w')
+with file1 as outfile:
+	json.dump(data, outfile, indent = 2)
 
 	
