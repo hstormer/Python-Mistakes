8476fceba77fa4c69baeff2611fc097aa9fe05bd
hstormer <hstormer@ualberta.com>
2016-08-10 13:04:36 -0600

    Made commitmessage work

--- WRITER6.py
+++ WRITER6.py
@@ -231,6 +231,7 @@ for i in filenames:
 					letterschanged += 1
 			m = thing.get("Message:")
 			m = m.encode('ascii', errors='ignore')
+			characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ']
 			for letter in m:
 				if not(str.lower(letter) in characters):
 					m = m.replace(letter, "")
