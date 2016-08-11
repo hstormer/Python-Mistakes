a84999fedf25d34de4692cdccc390d0f69f35fab
hstormer <hstormer@ualberta.com>
2016-08-11 11:14:44 -0600

    Changed the comment

--- DATACOLLECT.py
+++ DATACOLLECT.py
@@ -1,4 +1,5 @@
-# This initiates the thing that connects to the server thing that does a thing
+# Use this file to collect data about commits with and without mistakes. Pymongo must be installed, and there must be a mongo database of commits from github to look through.
+
 import pymongo
 import json
 from pymongo import MongoClient
